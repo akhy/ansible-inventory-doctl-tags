0fdec6bb2e17189b62ad060f31fcc93c330d71f0
Doug Thompson <service+git@dougthompson.co.uk>
2016-07-30 14:47:08 +0100

    the script in all its pythonic glory

--- doctl-tags.py
+++ doctl-tags.py
@@ -1,1 +1,37 @@
 #!/usr/bin/env python
+
+import os
+import subprocess
+import json
+
+def droplet_ip(droplet):
+    for interface in droplet['networks']['v4']:
+        if interface['type'] == 'public':
+            return interface['ip_address']
+
+droplets = json.loads(
+    subprocess.check_output([
+        'doctl',
+        'compute',
+        'droplet',
+        'list',
+        '--output',
+        'json',
+    ])
+)
+
+groups = {}
+ns = os.environ.get('DOCTL_INVENTORY_NAMESPACE')
+
+for droplet in droplets:
+    if ns and not droplet['name'].startswith(ns + '-'):
+        continue
+    for tag in droplet['tags'] + ['all']:
+        if groups.get(tag):
+            groups[tag]['hosts'].append(droplet_ip(droplet))
+        else:
+            groups[tag] = {}
+            groups[tag]['hosts'] = [droplet_ip(droplet)]
+            groups[tag]['vars'] = {}
+
+print(json.dumps(groups))
